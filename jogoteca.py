4f9dc4ae2258db2df47b5210712f98d14e954b2d
LGeremia <30410773+lgeremia@users.noreply.github.com>
2021-03-12 10:04:20 -0300

    Automatizando rotas e ajustando sistema de login

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,18 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+class Usuario:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+usuario1 =  Usuario('gere', 'Gere', 'teste')
+usuario2 =  Usuario('teste', 'teste', 'teste2')
+usuarios = {
+    usuario1.id: usuario1,
+    usuario2.id: usuario2
+}
 
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
@@ -22,7 +34,9 @@ def index():
 
 @app.route('/new')
 def new():
-    return render_template('new.html', titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado']==None:
+        return redirect(url_for('login',redirect=url_for('new')))
+    return render_template('new.html', title='Novo Jogo')
 
 @app.route('/create', methods=['POST',])
 def create():
@@ -31,26 +45,34 @@ def create():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    next_page = request.args.get('redirect')
+    return render_template('login.html',title='Login', next_page=next_page)
 
-@app.route('/authenticated', methods=['POST',])
-def authenticated():
-    if 'master' == request.form['senha']:
-        session['usuario_logado']=request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+@app.route('/authenticate', methods=['POST',])
+def authenticate():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        print('aqui')
+        if usuario.password == request.form['senha']:
+            next_page = request.form['next_page']
+            session['usuario_logado']=usuario.id
+            flash(usuario.username + ' logou com sucesso!')
+            return redirect(next_page)
+        else:
+            flash('Usuário ou senha inválidos!')
+            return redirect(url_for('login'))
     else:
-        flash('Senha inválida!')
-        return redirect('/login')
+        flash('Usuário ou senha inválidos!')
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout feito com sucesso!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
